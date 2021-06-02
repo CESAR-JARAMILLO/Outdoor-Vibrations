c924162b5158eadd863d144b4f5e85cf87ecadc3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-02 07:28:26 -0700

    created Navbar Component

---
+++ components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+    <b-navbar :centered="centered" :shadow="shadow">
+        <template #brand>
+            <b-navbar-item class="brand" tag="router-link" :to="{ path: '/' }">
+                Outdoor Vibrations
+            </b-navbar-item>
+        </template>
+        <template #start >
+            <b-navbar-item>
+                <NuxtLink to="/" class="nav-item">
+                    Home
+                </NuxtLink>
+            </b-navbar-item>
+            <b-navbar-item>
+                <NuxtLink to="/about" class="nav-item">
+                    About
+                </NuxtLink>
+            </b-navbar-item>
+            <b-navbar-item>
+                <NuxtLink to="/blogs" class="nav-item">
+                    Blog
+                </NuxtLink>
+            </b-navbar-item>
+        </template>
+
+        <template #end>
+            
+            <b-navbar-item tag="div">
+                <NuxtLink to="/subscribe">
+                    <div class="buttons">
+                        <a class="button">
+                            <strong>Subscribe</strong>
+                        </a>
+                    </div>
+                </NuxtLink>
+            </b-navbar-item>
+        </template>
+        <!-- <template #burger></template> -->
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            centered: true,
+            shadow: true
+        }
+    }
+}
+</script>
