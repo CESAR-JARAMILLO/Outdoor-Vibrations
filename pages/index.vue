9635288b2fc97c8f8903bbc6cb88fdb78a379b0e
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-02 07:26:52 -0700

    addedf stylying

--- pages/index.vue
+++ pages/index.vue
@@ -31,3 +31,88 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+.vector {
+  position: absolute;
+  bottom: 0rem;
+  left: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 50vh;
+}
+
+main {
+    margin-top: 40px;
+    height: 60vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    font-family: 'Montserrat', sans-serif;
+}
+
+h1 {
+    font-size: 64px;
+    width: 400px;
+    color: #40916C;
+    font-weight: 700;
+    background: -webkit-linear-gradient(#40916C, #58B727);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-family: 'Montserrat', sans-serif;
+}
+
+h2 {
+    font-size: 30px;
+    width: 400px;
+    font-weight: 400;
+}
+
+.left-col {
+    /* padding: 40px 0 40px 100px; */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.btn-container {
+    width: 440px;
+    display: flex;
+    justify-content: space-between;
+    /* margin-top: 20px; */
+}
+
+.right-col {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.right-col img {
+    /* margin-top: 50px; */
+    height: 28rem;
+}
+
+.btn {
+    /* margin-right: 2.5rem; */
+    /* margin-top: 1rem; */
+    background: linear-gradient(180deg, #58B727, #40916C);
+    color: #fff;
+    width: 200px;
+    height: 70px;
+    border: none;
+    border-radius: 5px;
+    font-size: 30px;
+}
+
+.footer-container {
+  margin-top: 7rem;
+}
+</style>
