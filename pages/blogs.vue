59446712dd75ebed7fe8da9f16c426a8e9214ad2
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-05 09:34:05 -0700

    added content

---
+++ pages/blogs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <Navbar />
+      <div class="featured">
+          <div class="featured-text">
+              <p class="featured-category">Featured</p>
+              <h2 class="featured-title">The outdoors, breaking down the walls and reconnecting.</h2>
+              <p class="featured-date">May 12, 2021</p>
+          </div>
+          <div class="featured-image">
+
+          </div>
+      </div>
+      <div class="blog-cards">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+      </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import BlogCard from '../components/BlogCard'
+export default {
+    name: 'blogs',
+    components: {
+        Navbar,
+        BlogCard
+    }
+}
+</script>
