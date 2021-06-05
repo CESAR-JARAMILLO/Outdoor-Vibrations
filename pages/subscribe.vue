4b22f40d7ec46a96d19c321157f01a4af3ee75d7
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-05 09:17:01 -0700

    added stylying

--- pages/subscribe.vue
+++ pages/subscribe.vue
@@ -31,3 +31,99 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+main {
+    margin-top: 40px;
+    height: 70vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    font-family: 'Roboto', sans-serif;
+}
+.left {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.right {
+    /* padding: 40px 0 40px 100px; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.right img {
+  height: 480px;
+  /* width: 35rem; */
+}
+
+.subscribe-card {
+  /* font-family: 'Roboto', sans-serif; */
+  /* margin-top: 3rem; */
+  /* margin-left: 2.5rem; */
+  padding: 0 18px;
+  height: 550px;
+  width: 450px;
+  border-radius: 10px;
+  text-align: center;
+  background-color: #40916C;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.subscribe-card h2 {
+    font-size: 44px;
+    font-weight: 600;
+}
+
+.subscribe-card p {
+    font-size: 20px;
+    font-weight: 400;
+}
+
+.subscribe-card form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 200px;
+    width: 300px;
+
+}
+
+form input {
+    width: 300px;
+    height: 50px;
+    border-radius: 5px;
+    border: none;
+}
+
+form button {
+    height: 40px;
+    width: 120px;
+    border-radius: 5px;
+    border: none;
+    background-color:   #58b727;
+    color: #fff;
+}
+
+.vector {
+  position: absolute;
+  bottom: 0rem;
+  left: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 50vh;
+  /* margin-bottom: 20px; */
+}
+</style>
