61eaaa008a6739e0c8fc3d9af609cbc283f71d62
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-05 09:32:12 -0700

    added content

---
+++ components/BlogCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+        <figure class="image is-4by3">
+            <img src="../assets/autumn.png" alt="Placeholder image">
+        </figure>
+    </div>
+    <div class="card-content">
+        <div class="media">
+            <div class="media-content">
+                <p class="card-category title is-6">Category</p>
+            </div>
+        </div>
+            <div class="content">
+                <p class="card-title title is-4">This is where the title goes</p>
+                <time class="card-date" datetime="2016-1-1">Jan 1, 2016</time>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogCard'
+}
+</script>
+
