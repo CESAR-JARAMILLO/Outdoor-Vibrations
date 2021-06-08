951440de72e31ed327c3549f62f0bfeb0422b119
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-08 10:06:21 -0700

    created formatDate function

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -2,7 +2,7 @@
   <article>
     <img src="../../assets/autumn.png" alt="">
     <nuxt-content :document="article" />
-    <p>Post last updated: {{ article.updatedAt }}</p>
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
     <pre> {{ article }} </pre>
   </article>
 </template>
@@ -13,6 +13,12 @@
       const article = await $content('articles', params.slug).fetch()
 
       return { article }
+    },
+    methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
     }
+ }
   }
 </script>
