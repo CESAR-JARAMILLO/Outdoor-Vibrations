39adcea7763c6d13e40c5cec6f1b451193615ce5
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-09 11:00:11 -0700

    added content and styled

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,82 +1,90 @@
 <template>
-  
-  <article>
+  <div>
     <Navbar />
-    <img class="main-image" :src="require(`~/assets/${article.img}`)" :alt="article.alt" />
-    <h1 class="title">{{ article.title }}</h1>
-    <p>Written by</p>
-    <hr>
-    <nuxt-content :document="article" />
-    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
-    <pre> {{ article }} </pre>
-  </article>
+    <article>
+      <img class="main-image" :src="require(`~/assets/${article.img}`)" :alt="article.alt">
+      <h1 class="title">
+        {{ article.title }}
+      </h1>
+      <div class="author">
+        <img class="author-image" :src="require(`~/assets/${article.author.image}`)">
+        <p>Christina Jules</p>
+      </div>
+      <hr>
+      <nuxt-content :document="article" />
+    </article>
+  </div>
 </template>
 
 <script>
-import Navbar from "../../components/Navbar"
+import Navbar from '../../components/Navbar'
 
-  export default {
-    async asyncData({ $content, params }) {
-      const article = await $content('articles', params.slug).fetch()
+export default {
+  components: {
+    Navbar
+  },
+  async asyncData ({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
 
-      return { article }
-    },
-    components: {
-      Navbar
-    },
-    methods: {
-    formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' }
-      return new Date(date).toLocaleDateString('en', options)
-    }
- }
+    return { article }
   }
+}
 </script>
 
-<style scoped>
-  article {
-    text-align: center;
-  }
-  .main-image {
-    margin-top: 3rem;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 60%;
+<style>
+article {
+  text-align: center;
+}
+
+.main-image {
+  margin-top: 3rem;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+}
+
+.title {
+  margin-top: 2rem;
+  font-size: 52px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
   }
 
-  .title {
-    margin-top: 2rem;
-    font-size: 52px;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
-  }
+hr {
+  margin: 1rem auto;
+  width: 50%;
+}
 
-  hr {
-    margin: 1rem auto;
-    width: 50%;
-  }
+.author {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .nuxt-content-container {
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
-  }
+.author-image {
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
 
-  p {
-    margin: 2rem 0;
-  }
+.nuxt-content {
+  width: 60%;
+  margin: auto;
+  text-align: left;
+}
 
-  .nuxt-content h1 {
-    font-weight: bold;
-    font-size: 28px;
-  }
-  .nuxt-content h3 {
-    font-weight: bold;
-    font-size: 22px;
-  }
-  .nuxt-content p {
-    margin-bottom: 20px;
-  }
+.nuxt-content p {
+  margin: 28px 0;
+}
+
+.nuxt-content h2 {
+  font-size: 40px;
+  margin-left: 0;
+}
+
+.icon.icon-link {
+  display: none;
+}
 </style>
