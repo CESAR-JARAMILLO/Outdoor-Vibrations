f189123bd512f76097cfc125b446f3d342d18468
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-09 13:17:59 -0700

    fixed eslint issues

--- pages/about.vue
+++ pages/about.vue
@@ -1,35 +1,35 @@
 <template>
   <div>
-    <Navbar/>
+    <Navbar />
     <main>
-        <div class="left">
-          <div class="about-card">
-            <h3>Passion</h3>
-            <h2>A journey of truth, with no destination.</h2>
-            <img src="../assets/mountain.png" alt="">
-            <p>Here at Outdoor Vibrations we believe that the road to growth should have no end. With many paths of improvement, inner growth and strength is achieved with every step.</p>
-            <p>Whether your connection to nature is vibrant, or are looking to taking your first steps. Adventure is better with company.</p>
-              <Nuxt-Link class="button" to="blogs">
-                <strong>Read Blog</strong>
-              </Nuxt-Link>
-          </div>
+      <div class="left">
+        <div class="about-card">
+          <h3>Passion</h3>
+          <h2>A journey of truth, with no destination.</h2>
+          <img src="../assets/mountain.png" alt="">
+          <p>Here at Outdoor Vibrations we believe that the road to growth should have no end. With many paths of improvement, inner growth and strength is achieved with every step.</p>
+          <p>Whether your connection to nature is vibrant, or are looking to taking your first steps. Adventure is better with company.</p>
+          <Nuxt-Link class="button" to="blogs">
+            <strong>Read Blog</strong>
+          </Nuxt-Link>
         </div>
-        <div class="right">
+      </div>
+      <div class="right">
         <img src="../assets/about-pic.png" alt="">
-        </div>
+      </div>
     </main>
     <img class="vector" src="../assets/vector.png" alt="">
     <!-- <TheFooter/> -->
-</div>
+  </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 export default {
-    name: 'about',
-    components: {
-        Navbar
-    }
+  name: 'About',
+  components: {
+    Navbar
+  }
 }
 </script>
 
