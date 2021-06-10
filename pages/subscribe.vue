b9ec54b8b6de75c8b6c2d9300bad7c2ffbca3495
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-10 10:15:58 -0700

    fixed eslint errors

--- pages/subscribe.vue
+++ pages/subscribe.vue
@@ -1,34 +1,33 @@
 <template>
   <div>
-    <Navbar/>
+    <Navbar />
     <main>
-        <div class="left">
-          <div class="subscribe-card">
-            <h2>Connect to our community</h2>
-            <p>Get email updates from Outdoor Vibrations and stay up-to-date on all of our new posts. We’ll also let you know when we have any special deals.</p>
-            <form action="">
-                <input type="text" placeholder="name">
-                <input type="email" placeholder="email">
-                <button>Subscribe</button>
-            </form>
-          </div>
+      <div class="left">
+        <div class="subscribe-card">
+          <h2>Connect to our community</h2>
+          <p>Get email updates from Outdoor Vibrations and stay up-to-date on all of our new posts. We’ll also let you know when we have any special deals.</p>
+          <form action="">
+            <input type="text" placeholder="name">
+            <input type="email" placeholder="email">
+            <button>Subscribe</button>
+          </form>
         </div>
-        <div class="right">
+      </div>
+      <div class="right">
         <img src="../assets/blogging.png" alt="">
-        </div>
+      </div>
     </main>
     <img class="vector" src="../assets/vector.png" alt="">
-    <!-- <TheFooter/> -->
-</div>
+  </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 export default {
-    name: 'subscribe',
-    components: {
-        Navbar
-    }
+  name: 'Subscribe',
+  components: {
+    Navbar
+  }
 }
 </script>
 
