0375bd90f8532b6d1973294576d2dc964940e6e8
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-06-10 09:59:33 -0700

    fixed eslint errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,29 @@
 <template>
-<div>
-  <Navbar/>
-  <main>
-        <div class="left-col">
-            <h1>Connecting to Nature</h1>
-            <h2>Have a love for all things that are outdoors?</h2>
-            <div class="btn-container">
-                <NuxtLink to="subscribe"><button class="btn" href="">Join Now</button></NuxtLink>
-                <NuxtLink to="blogs"><button class="btn" href="">Read Blog</button></NuxtLink>
-            </div>
+  <div>
+    <Navbar />
+    <main>
+      <div class="left-col">
+        <h1>Connecting to Nature</h1>
+        <h2>Have a love for all things that are outdoors?</h2>
+        <div class="btn-container">
+          <NuxtLink to="subscribe">
+            <button class="btn" href="">
+              Join Now
+            </button>
+          </NuxtLink>
+          <NuxtLink to="blogs">
+            <button class="btn" href="">
+              Read Blog
+            </button>
+          </NuxtLink>
         </div>
-        <div class="right-col">
-            <img src="../assets/landing-pic.png" alt="">
-        </div>
+      </div>
+      <div class="right-col">
+        <img src="../assets/landing-pic.png" alt="">
+      </div>
     </main>
     <img class="vector" src="../assets/vector.png" alt="">
-<!-- <TheFooter/> -->
-</div>
-
+  </div>
 </template>
 
 <script>
